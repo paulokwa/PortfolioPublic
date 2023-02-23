c8232e068d4444eeb047c6855d90c22954fe5912
paulokwa <71025398+paulokwa@users.noreply.github.com>
2023-02-23 14:23:13 -0400

    changes to navbar

--- js/sendForm.js
+++ js/sendForm.js
@@ -6,6 +6,8 @@ function sendToGoogle() {
         url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdPg0_xFoW2It9UBZv40wA8aPTwR05u9MzUuszlx2QZf3EM1Q/formResponse",
         data: {
             "entry.352931399": nameField,
+            "entry.1287098885": emailField,
+            "entry.75384403": messageField,
         },
         type: "POST",
         dataType: "xml",
