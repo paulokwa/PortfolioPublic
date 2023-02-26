5d0d58a2b34bdcf2e71ff3e13d4caf88fdc32698
Gustavo R. Malacarne <gustavomalacarne.r@gmail.com>
2023-02-26 15:36:27 -0300

    parallax text responsive v1.0

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ function calculateHeight() {
     parallax.style.minHeight = (parseFloat(width) * 0.577355229) + 'px'
 
     const fixed_text = document.querySelector(".fixed-text")
-    const height = $('.parallax').height();
-    //fixed_text.style.top = (height / (Math.sqrt(height) + 15)) + 'px'
+    const height = $( '.parallax' ).height();
+    fixed_text.style.top = ((height * 0.8) / 2) + 'px'
 }
 
